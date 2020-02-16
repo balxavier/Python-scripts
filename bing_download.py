5fec8c2b70e0fcd842ab3224f5c7a8fa9415579d
balxavier <60616373+balxavier@users.noreply.github.com>
2020-02-16 13:27:42 +0100

    Update bing_download.py

--- bing_download.py
+++ bing_download.py
@@ -1,14 +1,15 @@
 # Simple python script that download the bing daily site backgroud
-import os # import the os package
+import os, arexx # import the os and arexx packages
 
 drawer = 'ram:'
 
-os.system("wget -O ram:index.html http://www.bing.com") # download the bing index
+os.system("wget -q -O ram:index.html http://www.bing.com") # download the bing index
 str1 = open(drawer + 'index.html', 'r').read() # extract the file path using split
 str2=str1.split('background-image: url(')[1]
 str3=str2.split(".jpg")[0]
 str4=str3.split('/th?id=OHR.')[1]
 str5=str4.split('_FR-FR')[0]
 str6=drawer + str5 + '.jpg'
-os.system('wget -O ' + str6 + ' http://www.bing.com' + str3 + '.jpg') # donwload the daily image
+os.system('wget -q -O ' + str6 + ' http://www.bing.com' + str3 + '.jpg') # donwload the daily image
 os.remove(drawer + 'index.html') # delete the html file
+arexx.dorexx("NOTIFYA", "REGISTERAPP APP \"PYTHONRGTEST\" Image Dowloaded")
