dffddc990248b93b651d82decdbbbb5812831706
balxavier <60616373+balxavier@users.noreply.github.com>
2021-02-21 15:51:34 +0100

    Update AIOS_browser_extension.py

--- AIOS_browser_extension.py
+++ AIOS_browser_extension.py
@@ -1,7 +1,11 @@
 #!python
 # coding=utf-8
+# AIOStreams browser menu extension for AmigaOS
+# v1.0
 
-import os, sys, urllib, arexx
+import os, sys, myurlparse, arexx
+
+AIOSpath = os.path.dirname(__file__)
 
 #Read the url parsed threw the browser menu
 
@@ -9,41 +13,41 @@ url = sys.argv[1]
 
 #Extract Domain from url
 
-from urllib.parse import urlparse
+from myurlparse import urlparse
 domain = urlparse(url).netloc
 
 #Test the domain to determine the right script
 
-def urldomain(domain):
-        switcher={
-                'www.twitch.tv':'twitch',
-                'm.twitch.tv':'twitch',
-                'www.youtube.com':'youtube',
-                'youtu.be':'youtube',
-                'www.youtube-nocookie.com':'youtube',
-                'm.youtube.com':'youtube',
-                'youtube.com':'youtube',
-                'vimeo.com':'vimeo',
-                'www.dailymotion.com':'dailymotion',
-                'www.skaitv.gr':'skaitv',
-                'dlive.tv':'dlive',
-                'peertube.com':'peertube',
-                'wasd.tv':'wasd',
-                'lbry.tv':'lbry',
-             }
-         website = switcher.get(domain,"other")
+switcher = {
+	'www.twitch.tv': "twitch",
+	'm.twitch.tv': "twitch",
+	'www.youtube.com': "youtube",
+	'youtu.be': "youtube",
+	'www.youtube-nocookie.com': "youtube",
+	'm.youtube.com': "youtube",
+	'youtube.com': "youtube",
+	'vimeo.com': "vimeo",
+	'www.dailymotion.com': "dailymotion",
+	'www.skaitv.gr': "skaitv",
+	'dlive.tv': "dlive",
+	'peertube.com': "peertube",
+	'wasd.tv': "wasd",
+	'lbry.tv':"lbry"
+	}
+
+website = switcher.get(domain, "other")
 
 #Notify the result
 
-notifyarg1 = ' TITLE="' + website + ' video"'
-notifyarg2 = ' IMG="Icones/' + website + '.png" IMGVALIGN=1'
-arexx.dorexx("NOTIFYA", "REGISTERAPP APP=PYTHONNOTIFY" + notifyarg1 + notifyarg2 + "Now playing ...")
+notifyarg = 'SCREEN="FRONT" PRI=9 IMG="' + AIOSpath + '/Icones/' + website + '.png" TITLE="' + website + ' video" IMGVALIGN=1 Now Playing...'
+arexx.dorexx("NOTIFYA", "REGISTERAPP APP=PYTHONNOTIFY")
+arexx.dorexx("NOTIFYA", 'NOTIFY APP=PYTHONNOTIFY ' + notifyarg)
 
 #Launch the correct script
 
 if website == 'other':
-  os.system('APPDIR:Mplayer -cache 8092 " + url
+	os.system('APPDIR:Mplayer -cache 8092 ' + url)
 else:
-  os.system('C:Python ' + website + '.py ' + '-shh -u " + url
+	os.system('C:Python ' + AIOSpath + '/' + website + '.py ' + '-shh -u ' + url)
   
-areex.dorexx("NOTIFYA", "UNREGISTERAPP APP=PYTHONNOTIFY")
+arexx.dorexx("NOTIFYA", "UNREGISTERAPP APP=PYTHONNOTIFY")
